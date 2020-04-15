d83a38ad2fc045f4435f455717ce3dba3b8f5c77
Elvis Ning <ningelvis@gmail.com>
2020-04-15 16:42:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,21 @@
+require "yaml"
+require "pry"
+
+def load_library
+  # code goes here
 def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
+  emoticons = YAML.load_file(path)
+  lookup = {}
+  lookup[:get_meaning] = {}
+  lookup[:get_emoticon] = {}
+
+  emoticons.each do |word, pair|
+    japanese = pair[1]
+    american = pair[0]
+
+    lookup[:get_meaning][japanese] = word
+    lookup[:get_emoticon][american] = japanese
   end
-  emoji_hash
+
+  lookup
 end
