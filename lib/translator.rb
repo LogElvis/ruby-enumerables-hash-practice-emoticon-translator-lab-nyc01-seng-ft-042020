8ccaab55d742b06fbf8d5de96b8c82d094e1e2c8
Elvis Ning <ningelvis@gmail.com>
2020-04-15 16:33:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,5 @@
 require 'yaml'
-require 'pry'
+# require modules here
 
 def load_library
   # code goes here
@@ -13,20 +13,36 @@ def load_library(yaml_file)
   response
 end
 
-def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found" 
+def get_japanese_emoticon
+  # code goes here
+def get_japanese_emoticon(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_emoticon"].each do |english, japanese|
+    if emoticon == english
+      response = japanese
+    end
   end
-  result
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
 end
 
-def get_english_meaning(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_meaning"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found" 
+def get_english_meaning
+  # code goes here
+def get_english_meaning(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_meaning"].each do |emo, trans|
+    if emo == emoticon
+      response = trans
+    end
   end
-  result
-end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
+end 
