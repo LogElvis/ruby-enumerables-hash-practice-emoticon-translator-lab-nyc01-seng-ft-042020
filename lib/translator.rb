55e7dd287f622a008de316c0514656fe1fe8d1c7
Elvis Ning <ningelvis@gmail.com>
2020-04-15 16:28:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def load_library
   # code goes here
-    emoticons = YAML.load_file(path)
+  emoticons = YAML.load_file(path)
   emoticon_hash = Hash.new
 
   emoticon_hash["get_emoticon"] = Hash.new
