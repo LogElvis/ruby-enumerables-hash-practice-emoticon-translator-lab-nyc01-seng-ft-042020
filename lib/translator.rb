72c1b8a4de395910fc01f368385859a619b2e781
Elvis Ning <ningelvis@gmail.com>
2020-04-15 16:43:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,8 +27,6 @@ def get_japanese_emoticon(path, emoticon)
   end
 end
 
-def get_english_meaning
-  # code goes here
 def get_english_meaning(path, emoticon)
   lookup = load_library(path)
   if lookup[:get_meaning][emoticon]
